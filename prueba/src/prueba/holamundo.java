8c8b5061e00f1f94ab4c7f9535672484989f0c7c
kevintn <kevintn@172.20.10.3>
2017-02-13 12:15:01 +0000

    4

---
+++ prueba/src/prueba/holamundo.java
@@ -0,0 +1,12 @@
+package prueba;
+
+public class holamundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("BIENVENIDO MI REY esto es caca y yo me lo como");
+
+	}
+
+}
